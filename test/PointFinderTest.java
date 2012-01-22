dacdc745cda77a6d18727464aa14f8b0776156c7
Max Neverov <neverov.max@gmail.com>
2012-01-22 23:26:51 +0400

    extract methods

--- test/PointFinderTest.java
+++ test/PointFinderTest.java
@@ -1,8 +1,6 @@
 import junit.framework.TestCase;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -10,60 +8,58 @@ import java.util.List;
  */
 public class PointFinderTest extends TestCase {
     private double latitude = 11.123456, longitude = 22.654321;
+    private int pointId = 0;
+    private int routeId = 0;
 
-    public void test_for_empty_path() {
-        PointFinder pointFinder = new PointFinderImpl();
-        List<Point> pointList = new ArrayList<Point>();
-        Route emptyRoute = new Route(1, pointList);
-        List<Point> points = pointFinder.findPoints(emptyRoute, 100);
+    public void test_for_empty_route() {
+        PointFinder finder = createFinder();
+        Route emptyRoute = createRoute();
+        List<Point> points = finder.findPoints(emptyRoute, 100);
         assertTrue(points.isEmpty());
     }
 
     public void test_one_point_in_route() {
-        Point point = new Point(1, latitude, longitude);
-        List<Point> pointList = new ArrayList<Point>();
-        pointList.add(point);
-        PointFinder pointFinder = new PointFinderImpl(pointList);
-        Route onePointRoute = new Route(1, pointList);
-        List<Point> pointsForRoute = pointFinder.findPoints(onePointRoute, Integer.MAX_VALUE);
+        Point point = createPoint(latitude, longitude);
+        PointFinder finder = createFinder(point);
+        Route onePointRoute = createRoute(point);
+        List<Point> pointsForRoute = finder.findPoints(onePointRoute, Integer.MAX_VALUE);
         assertEquals(1, pointsForRoute.size());
         assertTrue(pointsForRoute.contains(point));
     }
 
     public void test_one_point_out_of_route() {
-        Point pointOutOfRoute = new Point(2, latitude, longitude+1);
-        List<Point> points = new ArrayList<Point>();
-        points.add(pointOutOfRoute);
-        PointFinder pointFinder = new PointFinderImpl(points);
-        Point point = new Point(1, latitude, longitude);
-        List<Point> pointList = new ArrayList<Point>();
-        pointList.add(point);
-        Route onePointRoute = new Route(1, pointList);
-        List<Point> pointsForRoute = pointFinder.findPoints(onePointRoute, Integer.MAX_VALUE);
+        Point pointOutOfRoute = createPoint(latitude, longitude + 1);
+        PointFinder finder = createFinder(pointOutOfRoute);
+        Route onePointRoute = createRoute(createPoint(latitude, longitude));
+        List<Point> pointsForRoute = finder.findPoints(onePointRoute, Integer.MAX_VALUE);
         assertEquals(1, pointsForRoute.size());
         assertTrue(pointsForRoute.contains(pointOutOfRoute));
     }
 
     public void test_points_sorted() {
-        Point point = new Point(1, latitude, longitude);
-        Point p1 = new Point(2, latitude + 1, longitude + 1);
-        Point p2 = new Point(3, latitude + 2, longitude + 2);
-        List<Point> pointsForRoute = new ArrayList<Point>();
-        pointsForRoute.add(point);
-        Route route = new Route(1, pointsForRoute);
-        List<Point> pointsOutOfRoute = new ArrayList<Point>();
-        pointsOutOfRoute.add(p2);
-        pointsOutOfRoute.add(p1);
-        PointFinder pointFinder = new PointFinderImpl(pointsOutOfRoute);
+        Point point = createPoint(latitude, longitude);
+        Route route = createRoute(point);
+        PointFinder pointFinder = createFinder(createPoint(latitude + 2, longitude + 2), createPoint(latitude + 1, longitude + 1));
         List<Point> result = pointFinder.findPoints(route, Integer.MAX_VALUE);
         assertTrue(point.distanceTo(result.get(0)) < point.distanceTo(result.get(1)));
     }
 
     public void test_not_return_duplicate_points() {
-        PointFinder finder = new PointFinderImpl(Collections.singletonList(new Point(1, latitude, longitude)));
-        Route route = new Route(1, Arrays.asList(new Point(2, latitude+1, longitude+1),
-                new Point(3, latitude+2, longitude+2)));
+        PointFinder finder = createFinder(createPoint(latitude, longitude));
+        Route route = createRoute(createPoint(latitude+1, longitude+1), createPoint(latitude+2, longitude+2));
         List<Point> result = finder.findPoints(route, Integer.MAX_VALUE);
         assertEquals(1, result.size());
+    }
+
+    private Route createRoute(Point... points) {
+        return new Route(routeId++, Arrays.asList(points));
+    }
+
+    private PointFinder createFinder(Point... points) {
+        return new PointFinderImpl(Arrays.asList(points));
+    }
+
+    private Point createPoint(double latitude, double longitude) {
+        return new Point(pointId++, latitude, longitude);
     }
 }
